999f2471042c6cfb883952ea1c3002ad9d3ae477
McB0y <rafael.cisneros.gomez@gmail.com>
2017-06-07 00:05:43 -0500

    More proves

--- inputs/src/app/app.component.ts
+++ inputs/src/app/app.component.ts
@@ -8,27 +8,12 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    public fecha: any
-    public hora:any
-    fechaCompletada: any;
+
 
     constructor(){
-        this.fecha = '';
-        this.hora = '';
+
     }
 
-    public campoFecha(fecha: any, hora: any){
-        this.fecha = fecha;
-        this.hora = hora;
-        this.fechaCompletada = `${fecha} ${hora}`;
-        console.log(this.fechaCompletada);
-    }
-    public campo(){
-        console.log(this.fecha);
-        console.log(this.hora)
-        this.fechaCompletada = `${this.fecha} ${this.hora}`;
-        console.log(this.fechaCompletada);
-    }
-
+    
 
 }
